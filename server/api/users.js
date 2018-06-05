f57be1634faa56766fc6518c0c731ffdca7626f6
Connor Jessup <cjessup@umich.edu>
2018-06-05 16:20:18 -0400

    favorite api routes on users.js

--- server/api/users.js
+++ server/api/users.js
@@ -12,3 +12,17 @@ router.get('/', (req, res, next) => {
     .then(users => res.json(users))
     .catch(next)
 })
+
+router.post('/:id/favorites', (req, res, next) => {
+  User.findById(req.params.id)
+    .then(user => user.addCocktail(req.body.cocktail))
+    .then(user => res.json(user))
+    .catch(next)
+});
+
+router.delete('/:id/favorites', (req, res, next) => {
+  User.findById(req.params.id)
+    .then(user => user.removeCocktail(req.body.cocktail))
+    .then(user => res.json(user))
+    .catch(next)
+});
